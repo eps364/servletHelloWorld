49e4b34429f6d4202f62499cbd791c3d714661bc
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-03-18 23:35:23 -0300

    Servlet + jsp

---
+++ WebContent/HelloWorld.jsp
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+	<div>
+
+		<form method="get" action="hello">
+			<p>
+				<label for="nome">Qual seu nome? </label> 
+				<input type="text" name="nome" />
+			</p>
+			<p>
+				<input type="submit" value="Enviar">
+			</p>
+
+		</form>
+
+	</div>
+
+</body>
+</html>
---
+++ src/br/dev/mission/HelloWorld.java
@@ -0,0 +1,32 @@
+package br.dev.mission;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.append("<html>");
+		out.append("<body>");
+		out.append("<h2> Hello World! " + request.getParameter("nome") + " </h2>" );
+		out.append("</body>");
+		out.append("</html>");
+	}
+
+}
